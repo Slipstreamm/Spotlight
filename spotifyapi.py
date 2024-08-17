c00800c8a0bff52de6bc481cdd4ed780a3ae1495
Slipstreamm <slippstreamm@pm.me>
2024-08-17 16:08:26 -0600

    lol exposed token

--- spotifyapi.py
+++ spotifyapi.py
@@ -1,5 +1,8 @@
 import requests
 
+def getProfile():
+    print('placeholder :p')
+
 def topItems(x, type, range, key):
     with requests.get(f'https://api.spotify.com/v1/me/top/{type}?limit=50&time_range={range}') as r:
         r.headers = f'Authorization: Bearer {key}'
