135d8faa6a52e7715e751e91ae1c74f99c08f74d
Slipstreamm <slippstreamm@pm.me>
2024-08-17 23:17:57 -0600

    b

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-from nicegui import ui
-import json
-from spotifyapi import *
+from nicegui import ui, app
+import json, spotifyapi
+from pprint import pprint
 green = '#3f9421'
 
 def appInfoExists():
-    try:
-        with open('settings.json') as f:
-            data = json.load(f)
-            return 'id' in data
-    except json.JSONDecodeError:
-        print('Error decoding JSON.')
-        return False
-    
-    
+    return 'id' in app.storage.general
+
 if(appInfoExists() == False):
     def saveDetails(id, secret):
-        with open('settings.json', mode='w') as f:
-            data = {'id': id, 'secret': secret}
-            json.dump(data, f)
-            ui.notify('Saved. You may close the dialog.')
+        app.storage.general['id'] = id
+        app.storage.general['secret'] = secret
 
     with ui.dialog(value=True) as d, ui.card():
         ui.label('Create an app at https://developer.spotify.com/dashboard and enter it\'s details here. They will be stored locally in plaintext.')
+        ui.label('http://127.0.0.1:1337/callback must be set as a redirect URI in your app.')
         with ui.row():
             t1 = ui.input(label='ID', placeholder='Your client id')
             t2 = ui.input(label='Secret', placeholder='Your client secret')
             ui.button('Save', on_click=lambda: saveDetails(t1.value, t2.value))
 
 with ui.header(elevated=True).style('background-color: #3f9421').classes('items-center justify-center'):
-    ui.label('Spotify Stats').style('font-size: 110%')
-    ui.link('Top Tracks', '/tracks').style('color: #FFFFFF').style('font-size: 110%')
-    ui.link('Top Artists', '/artists').style('color: #FFFFFF').style('font-size: 110%')
-    ui.link('Top Genres', '/tracks').style('color: #FFFFFF').style('font-size: 110%')
-    ui.link('Recent Streams', '/recent').style('color: #FFFFFF').style('font-size: 110%')
+    ui.link('Spotlight', '/home').style('font-size: 135%; text-decoration: none; color: #FFFFFF')
+    ui.link('Top Tracks', '/tracks').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
+    ui.link('Top Artists', '/artists').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
+    ui.link('Top Genres', '/tracks').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
+    ui.link('Recent Streams', '/recent').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
+    ui.button('Login with Spotify', on_click=lambda: spotifyapi.authorizeApp()).props('color=black')
 
-with ui.column(align_items='center') as c:
-    c.classes('justify-center items-start mx-auto')
-    c.style('padding-top: 50px')
-    ui.image(source='https://placehold.co/200x200').props('fit=scale-down').style('width: 200px; height: 200px;')
-    ui.label('Your Username Here').style('font-size: 110%')
+if(app.storage.general['loggedin'] == 0):
+    with ui.column(align_items='center') as c:
+        c.classes('justify-center items-start mx-auto')
+        c.style('padding-top: 50px')
+        #ui.image(source='https://placehold.co/200x200').props('fit=scale-down').style('width: 200px; height: 200px;')
+        ui.label('Login to start using Spotlight').style('font-size: 150%')
 
-ui.run(host='127.0.0.1', port=1337, native=False, favicon='📊', title='Spotify Stats', dark=True)
+if(app.storage.general['loggedin'] == 1):
+    p = spotifyapi.getProfile(app.storage.general['accesstoken'])   
+    with ui.column(align_items='center') as c:
+        c.classes('justify-center items-start mx-auto')
+        c.style('padding-top: 50px')
+        ui.image(source=p['images'][1]['url']).props('fit=scale-down').style('width: 200px; height: 200px;')
+        ui.label(p['display_name']).style('font-size: 120%')
+
+@ui.page(path='/callback', title='Spotlight', favicon='✅')
+def callback(code: str = None):
+    if(code != None):
+        ui.label('Authorization successful. You may now close this tab.').classes('justify-center items-center mx-auto')
+        app.storage.general['code'] = code
+        app.storage.general['loggedin'] = 1
+        spotifyapi.newToken()
+
+ui.run(host='127.0.0.1', port=1337, native=False, favicon='🔦', title='Spotify Stats', dark=True, storage_secret='123')
--- spotifyapi.py
+++ spotifyapi.py
@@ -1,9 +1,57 @@
-import requests
+import requests, base64, json
+from pprint import pprint
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from nicegui import ui, app
 
-def getProfile():
-    print('placeholder :p')
+def authorizeApp():
+    redirecturi = 'http://127.0.0.1:1337/callback'
+    scope = 'user-read-private user-read-recently-played user-top-read user-follow-read user-library-read playlist-read-private playlist-read-collaborative user-read-currently-playing user-read-playback-state'
+    id = app.storage.general['id']
+    ui.navigate.to(f'https://accounts.spotify.com/authorize?response_type=code&client_id={id}&scope={scope}&redirect_uri={redirecturi}', new_tab=True)
 
-def topItems(x, type, range, key):
-    with requests.get(f'https://api.spotify.com/v1/me/top/{type}?limit=50&time_range={range}') as r:
-        r.headers = f'Authorization: Bearer {key}'
-        return r
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in 
+    this function because it is programmed to be pretty 
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
+
+def newToken():
+    id = app.storage.general['id']
+    secret = app.storage.general['secret']
+    code = app.storage.general['code']
+    redirecturi = 'http://127.0.0.1:1337/callback'
+    authstring = f'{id}:{secret}'
+    authstring = authstring.encode('utf-8')
+    auth = base64.b64encode(authstring)
+    auth = auth.decode('utf-8')
+    with requests.post('https://accounts.spotify.com/api/token', {'code': code, 'redirect_uri': redirecturi, 'grant_type': 'authorization_code'}, headers={'content-type': 'application/x-www-form-urlencoded', 'Authorization': f'Basic {auth}'}) as r:
+        res = r.content.decode('utf-8')
+        res = json.loads(res)
+        app.storage.general['accesstoken'] = res['access_token']
+        app.storage.general['tokentype'] = res['token_type']
+        app.storage.general['scope'] = res['scope']
+        app.storage.general['expiresin'] = res['expires_in']
+        app.storage.general['refreshtoken'] = res['refresh_token']
+
+def refreshToken():
+    None
+
+def getProfile(key: str):
+    with requests.get(f'https://api.spotify.com/v1/me', headers={'Authorization': f'Bearer {key}'}) as r:
+        res = json.loads(r.text)
+        return res
+
+def topItems(x: int, type: str, range: str, key: str):
+    with requests.get(f'https://api.spotify.com/v1/me/top/{type}?limit={x}&time_range={range}', headers={'Authorization': f'Bearer {key}'}) as r: 
+        res = json.loads(r.text)
+        return res
