e3121300619a5e6032ba8029dde4855dcaf97556
Slipstreamm <slippstreamm@pm.me>
2024-08-17 23:52:57 -0600

    i hate git

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ import json, spotifyapi
 from pprint import pprint
 green = '#3f9421'
 
-def appInfoExists():
-    return 'id' in app.storage.general
-
-if(appInfoExists() == False):
-    def saveDetails(id, secret):
-        app.storage.general['id'] = id
-        app.storage.general['secret'] = secret
-
-    with ui.dialog(value=True) as d, ui.card():
-        ui.label('Create an app at https://developer.spotify.com/dashboard and enter it\'s details here. They will be stored locally in plaintext.')
-        ui.label('http://127.0.0.1:1337/callback must be set as a redirect URI in your app.')
-        with ui.row():
-            t1 = ui.input(label='ID', placeholder='Your client id')
-            t2 = ui.input(label='Secret', placeholder='Your client secret')
-            ui.button('Save', on_click=lambda: saveDetails(t1.value, t2.value))
-
 with ui.header(elevated=True).style('background-color: #3f9421').classes('items-center justify-center'):
     ui.link('Spotlight', '/home').style('font-size: 135%; text-decoration: none; color: #FFFFFF')
     ui.link('Top Tracks', '/tracks').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
@@ -27,20 +11,42 @@ with ui.header(elevated=True).style('background-color: #3f9421').classes('items-
     ui.link('Recent Streams', '/recent').style('color: #FFFFFF').style('font-size: 110%; text-decoration: none')
     ui.button('Login with Spotify', on_click=lambda: spotifyapi.authorizeApp()).props('color=black')
 
-if(app.storage.general['loggedin'] == 0):
-    with ui.column(align_items='center') as c:
-        c.classes('justify-center items-start mx-auto')
-        c.style('padding-top: 50px')
-        #ui.image(source='https://placehold.co/200x200').props('fit=scale-down').style('width: 200px; height: 200px;')
-        ui.label('Login to start using Spotlight').style('font-size: 150%')
+@ui.refreshable
+def homepage():
+    def appInfoExists():
+        if(app.storage.general['id'] == ""):
+            return False
 
-if(app.storage.general['loggedin'] == 1):
-    p = spotifyapi.getProfile(app.storage.general['accesstoken'])   
-    with ui.column(align_items='center') as c:
-        c.classes('justify-center items-start mx-auto')
-        c.style('padding-top: 50px')
-        ui.image(source=p['images'][1]['url']).props('fit=scale-down').style('width: 200px; height: 200px;')
-        ui.label(p['display_name']).style('font-size: 120%')
+    if(appInfoExists() == False):
+        def saveDetails(id, secret):
+            app.storage.general['id'] = id
+            app.storage.general['secret'] = secret
+            ui.notify('Info saved, you may close the dialog')
+
+        with ui.dialog(value=True) as d, ui.card():
+            ui.label('Create an app at https://developer.spotify.com/dashboard and enter it\'s details here. They will be stored locally in plaintext.')
+            ui.label('http://127.0.0.1:1337/callback must be set as a redirect URI in your app.')
+            with ui.row():
+                t1 = ui.input(label='ID', placeholder='Your client id')
+                t2 = ui.input(label='Secret', placeholder='Your client secret')
+                ui.button('Save', on_click=lambda: saveDetails(t1.value, t2.value))
+
+    if(app.storage.general['loggedin'] == 0):
+        with ui.column(align_items='center') as c:
+            c.classes('justify-center items-start mx-auto')
+            c.style('padding-top: 50px')
+            #ui.image(source='https://placehold.co/200x200').props('fit=scale-down').style('width: 200px; height: 200px;')
+            ui.label('Login to start using Spotlight').style('font-size: 150%')
+
+    if(app.storage.general['loggedin'] == 1):
+        p = spotifyapi.getProfile(app.storage.general['accesstoken'])   
+        with ui.column(align_items='center') as c:
+            c.classes('justify-center items-start mx-auto')
+            c.style('padding-top: 50px')
+            ui.image(source=p['images'][1]['url']).props('fit=scale-down').style('width: 200px; height: 200px;')
+            ui.label(p['display_name']).style('font-size: 120%')
+
+homepage()
 
 @ui.page(path='/callback', title='Spotlight', favicon='✅')
 def callback(code: str = None):
@@ -49,5 +55,6 @@ def callback(code: str = None):
         app.storage.general['code'] = code
         app.storage.general['loggedin'] = 1
         spotifyapi.newToken()
-
+        homepage.refresh()
+        
 ui.run(host='127.0.0.1', port=1337, native=False, favicon='🔦', title='Spotify Stats', dark=True, storage_secret='123')
